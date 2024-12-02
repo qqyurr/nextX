8a330f473abe3ac2ecf5046f772382c6e18121b0
gyuyeon <byyulli16@gmail.com>
2024-12-02 13:37:40 +0900

    feat: after login page

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
 import Main from "@/app/(beforeLogin)/_component/Main";
 
 export default function Login() {
   const router = useRouter();
-  router.replace("/i/flow/login");
-  return <Main />;
+  router.replace('/i/flow/login');
+  return (
+    <Main/>
+  );
 }
